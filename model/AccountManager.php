da060aa4301d98ff4b41277a5062edf00e3ad4dd
Clement Sanchez <sanchez.clmnt@gmail.com>
2017-11-06 16:22:58 +0100

    make the metho getAccounts for the class AccountManager

--- model/AccountManager.php
+++ model/AccountManager.php
@@ -19,5 +19,20 @@ class AccountManager
   {
       $this->bdd=$bdd;
   }
+
+  /** Return all the vehicules in database
+   *@param empty
+   *@return array of objects
+   */
+  public function getAccounts() {
+    $accounts = [];
+    $reponse = $this->bdd->query('SELECT * FROM Accounts ORDER BY date_modif DESC');
+
+    while ($donnees = $reponse->fetch(PDO::FETCH_ASSOC)) {
+      $accounts[] = new Account($donnees);
+    }
+
+    return $accounts;
+  }
 }
  ?>
